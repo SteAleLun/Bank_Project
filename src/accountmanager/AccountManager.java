3511eb5ca7c85ef6abe1597b4df91ddaf89fe524
SteAleLun <stealelun@gmail.com>
2023-04-12 18:37:43 +0400

    ...

--- src/accountmanager/AccountManager.java
+++ src/accountmanager/AccountManager.java
@@ -14,20 +14,14 @@ public class AccountManager {
     public void transaction(BankAccount sender, BankAccount receiver, int transferAmount)
             throws BankAccountIsNotActiveException, InsufficientFundsException {
 
-        if (!sender.isActive()) {
-            throw new BankAccountIsNotActiveException("Счёт отправителя неактивен");
-        } else if (!receiver.isActive()) {
-            throw new BankAccountIsNotActiveException("Счёт получателя неактивен");
-        } else {
-            if (sender.getAmount() < transferAmount) {
-                throw new InsufficientFundsException("Сумма перевода превышает доступную сумму средств на счету");
-            } else {
-                sender.setAmount(sender.getAmount() - transferAmount);
-                receiver.setAmount(receiver.getAmount() + transferAmount);
-            }
-        }
+        checkAccountIsActive(sender, receiver);
+        checkTransactionIsPossible(sender, transferAmount);
+
+        sender.setAmount(sender.getAmount() - transferAmount);
+        receiver.setAmount(receiver.getAmount() + transferAmount);
 
     }
+
 
     // вернуть список НЕактивных счетов
     public ArrayList<BankAccount> getInactiveAccounts(ArrayList<BankAccount> accounts) {
@@ -55,10 +49,26 @@ public class AccountManager {
             }
         }
 
-        if(account == null) throw new BankAccountIdNotFoundException("Счёта с id: '"
+        if (account == null) throw new BankAccountIdNotFoundException("Счёта с id: '"
                 + id + "' не существует");
 
         return account;
+    }
+
+    // проверка, активны ли аккаунты
+    private void checkAccountIsActive(BankAccount sender, BankAccount receiver) throws BankAccountIsNotActiveException {
+        if (!sender.isActive()) {
+            throw new BankAccountIsNotActiveException("Счёт отправителя неактивен");
+        } else if (!receiver.isActive()) {
+            throw new BankAccountIsNotActiveException("Счёт получателя неактивен");
+        }
+    }
+
+    // проверка, достаточно ли средств для перевода
+    private void checkTransactionIsPossible(BankAccount sender, int transferAmount) throws InsufficientFundsException {
+        if (sender.getAmount() < transferAmount) {
+            throw new InsufficientFundsException("Сумма перевода превышает доступную сумму средств на счету");
+        }
     }
 
 }
